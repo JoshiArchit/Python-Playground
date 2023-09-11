becff15b40db1a83d3aa4c0b94e45579f2443dce
architJoshi2904 <aj6082@rit.edu>
2023-09-11 18:30:56 -0400

    Implementing queues using list

--- queuePy.py
+++ queuePy.py
@@ -1,5 +1,5 @@
 """
-Filename :
+Filename : queuePy
 Author : Archit Joshi
 Description : Implementing queue datastructure using python list (FIFO)
 Language : python3
@@ -7,24 +7,55 @@ Language : python3
 
 
 class Queue:
+    """
+    Implementing the queue datastructure from scratch using lists.
+    """
+
     def __init__(self):
+        """
+        Instantiate empty queue using list.
+        """
         self.queue = []
 
     def isEmpty(self):
+        """
+        Check if the queue is empty.
+
+        :return: True if the list/queue is empty
+        """
         return len(self.queue) == 0
 
     def enqueue(self, data):
+        """
+        Insert data into queue.
+
+        :param data: data
+        :return: None
+        """
         self.queue.append(data)
 
     def dequeue(self):
+        """
+        Delete item from the front of queue (the first element in the list)
+        :return: None if the queue is empty
+        """
         if self.isEmpty():
             return None
         self.queue.remove(0)
 
     def size(self):
+        """
+        Return length of queue
+        :return: length of queue
+        """
         return len(self.queue)
 
     def isFront(self):
+        """
+        Return the element in the front of the queue.
+
+        :return: first element in the list
+        """
         if self.isEmpty():
             return None
         return self.queue[0]
